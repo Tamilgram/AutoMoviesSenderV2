c01e9dfcf25f0ef3a6200481d559c1b0094294a6
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-08-13 18:18:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 from info import *
 class script(object):
-    START_TXT = """{CUSTOM_WELCOME}"""
+    START_TXT = """Hi {}"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
