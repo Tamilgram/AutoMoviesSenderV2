b9a0f40e841e7b6acb0ffe89f0214478e6282481
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2022-08-13 17:50:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,7 +63,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", url=f'https://{WEBSITE}/st?api={API_KEY}&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}'
                 ),
             ]
             for file in files
@@ -72,11 +72,11 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{file.file_name}", url=f'https://{WEBSITE}/st?api={API_KEY}&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}'
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'files_#{file.file_id}',
+                    url=f'https://{WEBSITE}/st?api={API_KEY}&url=https://t.me/{temp.U_NAME}?start=files_{file.file_id}',
                 ),
             ]
             for file in files
